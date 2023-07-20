467896a35eacfe29ad847edb367d514b8b9bdeee
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-20 15:52:44 +0600

    connect: connect to mongodb

---
+++ db/index.js
@@ -0,0 +1,32 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gzw4htn.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+// Connect to MongoDB and initialize collections
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+const db = client.db('graphql');
+const userCollection = db.collection('user');
+
+
+module.exports = {
+    connectToMongoDB,
+    userCollection
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = process.env.PORT || 5000
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('graphql server is running')
+})
+
+app.listen(port,()=>console.log('listeting on port '+port))
