0f6a1189ac436420b9b6c2ef80af0209d5ce3fdd
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-21 11:14:51 +0600

    graphql added successfully

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,6 +1,12 @@
+const {userCollection} = require('./../db/index')
 const resolvers = {
     Query:{
-        hello:()=> 'Hello World'
+        hello:()=> 'Hello World',
+        Ur:async()=>{
+            const query = {}
+            const result = await userCollection.find(query).toArray()
+            return result
+        }
     }
 }
 
--- gql/schema.js
+++ gql/schema.js
@@ -1,7 +1,15 @@
 const {gql} = require('apollo-server-express')
 const typeDefs = gql`
+
+    type UserReturn{
+        _id:String
+        name:String
+        email:String
+    }
+
     type Query {
         hello: String
+        Ur:[UserReturn]
     }
 `
 
