0465647b46fdbbdd36057db86ab80d2fb2b4c51d
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-25 11:01:45 +0600

    graphql server is ready

--- gql/schema/index.js
+++ gql/schema/index.js
@@ -1,15 +1,14 @@
-const {gql} = require('apollo-server-express')
-const Ur = require('./User')
-const hello = require('./hello')
+const { gql } = require("apollo-server-express");
+const Ur = require("./User");
+const hello = require("./hello");
 const rootSchmea = gql`
+  type Query {
+    _: Boolean
+  }
 
-    type Query {
-        _:Boolean
-    }
+  type Mutation {
+    _: Boolean
+  }
+`;
 
-    type Mutation{
-        _:Boolean
-    }
-`
-
-module.exports = [rootSchmea, Ur,hello]
+module.exports = [rootSchmea, Ur, hello];
